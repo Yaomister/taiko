275948b93109629a4cf5157cdc91bbf4912c2e24
ang9009 <ngalvin2004@gmail.com>
2026-04-08 14:41:01 -0400

    Add updated job, logs, and trained models

--- jobs/job.sh
+++ jobs/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=taiko_cnn
-#SBATCH --output=logs/taiko_cnn_%j.out
-#SBATCH --error=logs/taiko_cnn_%j.err
+#SBATCH --job-name=bigka
+#SBATCH --output=logs/bigka_cnn_%j.out
+#SBATCH --error=logs/bigka_cnn_%j.err
 #SBATCH --time=8:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -9,6 +9,7 @@
 #SBATCH --cpus-per-task=4
 
 python model/training.py \
-    --data_dir data/preprocessed/exports/ese \
-    --out test_model/test.pth \
-    --epochs 20
+    --data_dir data/preprocessed/exports/ese_bigka \
+    --out binary/bigka.pth \
+    --epochs 100 \
+    --lr 0.003
