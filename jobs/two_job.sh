eba8f662410ca0a41ac0fb6e638b747dee14474a
Eric Yao <ericyaohan@yahoo.com>
2026-04-09 23:04:38 -0400

    pls work

--- jobs/two_job.sh
+++ jobs/two_job.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=train_don_ka
-#SBATCH --output=logs/train_don_ka_%j.out
-#SBATCH --error=logs/train_don_ka_%j.err
+#SBATCH --job-name=train_two
+#SBATCH --output=logs/train_two_%j.out
+#SBATCH --error=logs/train_two_%j.err
 #SBATCH --time=8:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
 #SBATCH --cpus-per-task=4
 
-
-
-source /home/yao.eric/venv/taiko/bin/activate
-
-
-mkdir -p models
-
-
 python model/training.py \
---data_dir data/preprocessed/exports/ese_bigka \
---out binary/bigka.pth \
---epochs 100 \
---lr 0.003
+    --data_dir data/preprocessed/exports/ese_don_ka \
+    --out models/two.pth \
+    --epochs 100 \
+    --lr 0.003
