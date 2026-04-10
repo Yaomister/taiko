6846abbd7b9f4d76eb18a248f805c2aca2a54411
Eric Yao <ericyaohan@yahoo.com>
2026-04-09 22:29:15 -0400

    added jobs for training binary and trinary classifier models

---
+++ jobs/three_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=train_don_ka
+#SBATCH --output=logs/train_don_ka_%j.out
+#SBATCH --error=logs/train_don_ka_%j.err
+#SBATCH --time=8:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=4
+
+
+mkdir -p models
+
+
+python model/training.py \
+  --data_dir data/preprocessed/exports/ese_don_ka \
+  --out models/don_ka.pth \
+  --epochs 100 \
+  --lr 0.003
---
+++ jobs/two_dataset_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=build_don_ka
+#SBATCH --output=logs/build_don_ka_%j.out
+#SBATCH --error=logs/build_don_ka_%j.err
+#SBATCH --time=8:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=4
+
+
+./data/src/build_dataset.sh -d normal -f ese_don -n don -b 500 -r 0.25
